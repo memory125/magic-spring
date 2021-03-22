b4aaa96835b67abd470244dfdfcf5ef5d0f2abe6
memory125 <mengde1120@sina.com>
2021-03-22 15:48:29 +0800

    create spring-annotation-06

---
+++ spring-branch/spring-annotation-06/src/main/java/com/wing/pojo/User.java
@@ -0,0 +1,54 @@
+package com.wing.pojo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author memory125
+ */
+// @Component 等价于<bean id="user" class="com.wing.pojo.User" />
+@Component
+public class User {
+    @Value("Tom")
+    private String name;
+    @Value("1")
+    private int id;
+    @Value("36")
+    private int age;
+    @Value("Male")
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
