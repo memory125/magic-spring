f5b837c4c5a1b6307f0cfb5f02e63cf06467192e
memory125 <mengde1120@sina.com>
2021-03-30 18:16:43 +0800

    add UserController

---
+++ spring-mvc/springmvc-04-controller/src/main/java/com/wing/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.wing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pojo.User;
+
+
+/**
+ * @author memory125
+ */
+@Controller
+public class UserController {
+
+    // http://localhost:8080/user/u1?name=Richard
+    @GetMapping("/user/u1")
+    public String testUser1(String name, Model model) {
+        model.addAttribute("msg", name);
+        return "test";
+    }
+
+    // http://localhost:8080/user/u2?id=1&name=Richard&age=3
+    @GetMapping("/user/u2")
+    public String testUser2(User user, Model model) {
+        System.out.println(user);
+        model.addAttribute("msg", user);
+        return "test";
+    }
+}
