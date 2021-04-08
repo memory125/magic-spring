d492e57e51bd888d2154fb7246b3250a40385b34
memory125 <mengde1120@sina.com>
2021-04-08 14:53:29 +0800

    update Staff

--- spring-boot/springboot-02-configuration/src/main/java/com/wing/pojo/Staff.java
+++ spring-boot/springboot-02-configuration/src/main/java/com/wing/pojo/Staff.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,11 +13,18 @@ import java.util.Map;
 /**
  * @author memory125
  */
+/*
+    @ConfigurationProperties作用：
+    将配置文件中配置的每一个属性的值，映射到这个组件中；
+    告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+    参数 prefix = “staff” : 将配置文件中的staff下面的所有属性一一对应
+*/
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
-@ConfigurationProperties(prefix = "staff")
+@PropertySource("classpath:staff.yml") // 加载指定的配置文件
+@ConfigurationProperties(prefix = "staff")  // 默认从全局配置文件中获取值
 public class Staff {
     private String name;
     private int age;
